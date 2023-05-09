27fd8ada127bf4b66306df4d784619cfb4e787fe
John Cawley <JCawley@carletoninc.com>
2023-05-09 17:06:05 -0400

    Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  <div>
+    <h1>Hello world!</h1>
+  </div>;
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
